f25c6f512f1d26301df9eb57b45e99c439e4daee
Rob Carrington <rob@carbonos.dev>
2024-09-17 13:55:52 -0400

    fix: fix incorrect name references

--- apps/carbon/app/modules/inventory/ui/Inventory/InventoryDetails.tsx
+++ apps/carbon/app/modules/inventory/ui/Inventory/InventoryDetails.tsx
@@ -117,7 +117,7 @@ const InventoryDetails = ({
           <CardHeader className="pb-8">
             <CardDescription>Quantity on Purchase Order</CardDescription>
             <CardTitle className="text-4xl">
-              {formatter.format(quantities.quantityOnPurchaseOrder ?? 0)}
+              {formatter.format(quantities.quantityOnPurchase ?? 0)}
             </CardTitle>
           </CardHeader>
         </Card>
@@ -133,7 +133,7 @@ const InventoryDetails = ({
           <CardHeader className="pb-8">
             <CardDescription>Quantity on Production Order</CardDescription>
             <CardTitle className="text-4xl">
-              {formatter.format(quantities.quantityOnProdOrder ?? 0)}
+              {formatter.format(quantities.quantityOnProductionOrder ?? 0)}
             </CardTitle>
           </CardHeader>
         </Card>
